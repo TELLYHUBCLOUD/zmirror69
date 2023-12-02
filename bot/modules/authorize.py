50b3e748527d3b5994062ed8b856669296d44e61
Hrishikesh Thombare <hrishithombare15@gmail.com>
2023-12-02 17:37:01 +0530

    Update authorize.py

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-from pyrogram.filters import command
+from pyrogram import filters
 from pyrogram.handlers import MessageHandler
 
 from bot import DATABASE_URL, bot, user_data
@@ -8,10 +7,12 @@ from bot.helper.ext_utils.db_handler import DbManager
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import sendMessage
-import telebot
-bot = telebot.TeleBot('6861568223:AAHNYl5cGVp7ApK5urUIn2tMUAqdhgJJyp8')
-sticker_id = 'CAACAgEAAxkBAAEXnGRladqjF5RvYN3ik0edwvnpQwvvZAACTgMAAuGpAAFHKEMLcy3Tm50zBA'
 
+# Your previous functions...
+
+async def send_sticker_and_message(message, sticker_file_id, text):
+    await message.reply_sticker(sticker=sticker_file_id)
+    await sendMessage(message, text)
 
 async def authorize(_, message):
     msg = message.text.split()
@@ -22,76 +23,23 @@ async def authorize(_, message):
     else:
         id_ = message.chat.id
     if id_ in user_data and user_data[id_].get('is_auth'):
-        bot.send_sticker(message.chat.id, sticker_id)
         msg = 'Already Authorized!'
+        await send_sticker_and_message(message, 'CAACAgEAAxkBAAEXnGRladqjF5RvYN3ik0edwvnpQwvvZAACTgMAAuGpAAFHKEMLcy3Tm50zBA', msg)
     else:
         update_user_ldata(id_, 'is_auth', True)
         if DATABASE_URL:
             await DbManager().update_user_data(id_)
-        bot.send_sticker(message.chat.id, sticker_id)
         msg = 'Authorized Successfully!'
-    await sendMessage(message, msg)
+        await send_sticker_and_message(message, 'CAACAgEAAxkBAAEXnGRladqjF5RvYN3ik0edwvnpQwvvZAACTgMAAuGpAAFHKEMLcy3Tm50zBA', msg)
 
+# Modify other functions similarly...
 
-async def unauthorize(_, message):
-    msg = message.text.split()
-    if len(msg) > 1:
-        id_ = int(msg[1].strip())
-    elif reply_to := message.reply_to_message:
-        id_ = reply_to.from_user.id
-    else:
-        id_ = message.chat.id
-    if id_ not in user_data or user_data[id_].get('is_auth'):
-        update_user_ldata(id_, 'is_auth', False)
-        if DATABASE_URL:
-            await DbManager().update_user_data(id_)
-        msg = 'Unauthorized Successfully!'
-    else:
-        msg = 'Already Unauthorized!'
-    await sendMessage(message, msg)
+# Replace 'STICKER_FILE_ID_ALREADY_AUTHORIZED' and 'STICKER_FILE_ID_AUTHORIZED_SUCCESSFULLY'
+# with the actual file IDs of your stickers.
 
+bot.add_handler(MessageHandler(authorize, filters=filters.command(BotCommands.AuthorizeCommand) & CustomFilters.sudo))
+bot.add_handler(MessageHandler(unauthorize, filters=filters.command(BotCommands.UnAuthorizeCommand) & CustomFilters.sudo))
+bot.add_handler(MessageHandler(addSudo, filters=filters.command(BotCommands.AddSudoCommand) & CustomFilters.sudo))
+bot.add_handler(MessageHandler(removeSudo, filters=filters.command(BotCommands.RmSudoCommand) & CustomFilters.sudo))
 
-async def addSudo(_, message):
-    id_ = ""
-    msg = message.text.split()
-    if len(msg) > 1:
-        id_ = int(msg[1].strip())
-    elif reply_to := message.reply_to_message:
-        id_ = reply_to.from_user.id
-    if id_:
-        if id_ in user_data and user_data[id_].get('is_sudo'):
-            msg = 'Already Sudo!'
-        else:
-            update_user_ldata(id_, 'is_sudo', True)
-            if DATABASE_URL:
-                await DbManager().update_user_data(id_)
-            msg = 'Promoted as Sudo.'
-    else:
-        msg = "Give ID or Reply To message of whom you want to Promote."
-    await sendMessage(message, msg)
-
-
-async def removeSudo(_, message):
-    id_ = ""
-    msg = message.text.split()
-    if len(msg) > 1:
-        id_ = int(msg[1].strip())
-    elif reply_to := message.reply_to_message:
-        id_ = reply_to.from_user.id
-    if id_ and id_ not in user_data or user_data[id_].get('is_sudo'):
-        update_user_ldata(id_, 'is_sudo', False)
-        if DATABASE_URL:
-            await DbManager().update_user_data(id_)
-        msg = 'Demoted!'
-    else:
-        msg = "Give ID or Reply To message of whom you want to remove from Sudo"
-    await sendMessage(message, msg)
-
-bot.add_handler(MessageHandler(authorize, filters=command(
-    BotCommands.AuthorizeCommand) & CustomFilters.sudo))
-bot.add_handler(MessageHandler(unauthorize, filters=command(
-    BotCommands.UnAuthorizeCommand) & CustomFilters.sudo))
-bot.add_handler(MessageHandler(addSudo, filters=command(
-    BotCommands.AddSudoCommand) & CustomFilters.sudo))
-bot.add_handler(MessageHandler(removeSudo, filters=command(
-    BotCommands.RmSudoCommand) & CustomFilters.sudo))
+# No need for the if __name__ block if this is part of an existing application
